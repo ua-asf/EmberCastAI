d8c9b8c2d334a18da0d1984cf01a2c9ba0b01542
Dylan Maltos <dmmaltos@alaska.edu>
2025-06-09 12:09:55 -0800

    Push

--- get_fires_and_extremes.py
+++ get_fires_and_extremes.py
@@ -1,53 +1,58 @@
-#!python
-
 import os
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
 import asf_search as asf
+import numpy as np
+import shutil
 
 # Dates used by the dataset are usually in the format YYYYMMDD
-date_format_str = '%Y%m%d'
-
-# Parse /dataset/coordinates folder
+date_format_str = '%Y-%m-%d-%H%M'
 
 # Fires is a dict of each fire, with each fire being a 
 # list of the files associated with that fire, sorted
 # by date
-fires = {}
+fires = dict()
 
-for dirpath, dirnames, filenames in os.walk('dataset/coordinates'):
-    for filename in filenames:
-        # Search for files with the .wkt extension
-        if filename.endswith('.wkt'):
-            file = os.path.join(dirpath, filename)
-            # Insert into fires list
-            # Search for last list that contains the same beginning filepath
-            fire_found = False
-            for fire in fires.keys():
-                if file.startswith(fire):
-                    fires[fire].append(file)
-                    fire_found = True
-                    break
+# Grab all fires (they are organized as organized_dataset/<FIRENAME>/<FILES>)
+dirnames = os.listdir('organized_dataset')
+for dirname in dirnames:
+    # Check if the directory is a fire
+    if os.path.isdir(os.path.join('organized_dataset', dirname)):
+        try:
+            # Get all folder in the directory
+            dates = os.listdir(os.path.join('organized_dataset', dirname))
+            # Purge any 'unknown date' folders
+            dates = [date for date in dates if date != 'UnknownDate' and date != 'data']
+            # Sort the files by date
+            dates.sort(key=lambda x: datetime.strptime(x, date_format_str))
+            # Skip any fires with less than 2 days of data
+            if len(dates) < 2:
+                print(f'Skipping {dirname} with {len(dates)} days of data')
+                continue
+            # Add the fire to the fires dict
+            fires[dirname] = [os.path.join('organized_dataset', dirname, file) for file in dates]
+        except Exception as e:
+            print(f'Error with {dirname}: {e}')
+            continue
 
-            # In the case of a new fire being found, add it to the fires dict
-            if not fire_found:
-                file_dir = file.split('/')
+# Purge any fires not in the fires dict
+for fire in dirnames:
+    if fire not in fires:
+        # Check if the directory is a fire
+        if os.path.isdir(os.path.join('organized_dataset', fire)):
+            # Remove the directory
+            shutil.rmtree(os.path.join('organized_dataset', fire), ignore_errors=True)
+            print(f'Removed {fire}')
 
-                for i in range(len(file_dir)):
-                    # All days for a fire are the same except for after the 'IR' or 'KML_KMZ' folder
-                    if file_dir[i].upper() in ['IR', 'KML_KMZ', 'KMZ', 'GIS']:
-                        file_dir = file_dir[:i]
-                        break
-                    else:
-                        try:
-                            # See if the current string is a date, at which point the fire name is found
-                            datetime.strptime(file_dir[i], date_format_str)
-                            file_dir = file_dir[:i]
-                            break
-                        except ValueError:
-                            continue
+print(f'Found {len(fires)} fires')
 
-                fires['/'.join(file_dir)] = [file]
+# Purge empty fires
+fires = {fire: data for fire, data in fires.items() if len(data) > 0}
+
+print(fires)
+
+# Correct fires such that each pixel is 20m
+
 
 # Find the lat/long extremes for each fire
 
@@ -56,36 +61,41 @@ for dirpath, dirnames, filenames in os.walk('dataset/coordinates'):
 extremes = fires.copy()
 
 for fire_name, data in fires.items():
-    lat_min = 180
-    lat_max = -180
-    long_min = 180
-    long_max = -180
+    for day in data:
+        for file in os.listdir(day):
+            # Only open the .wkt files
+            if not file.endswith('.wkt'):
+                continue
 
-    for file in data:
-        # Parse the lat/long from the file
-        with open(file, 'r') as f:
-            # Every line is a polygon
-            for line in f.readlines():
-                # Remove the 'POLYGON ((' and '))\n' from the line
-                line = line.removeprefix('POLYGON ((').removesuffix('))\n')
-                coords = line.split(', ')
-                for line in coords:
-                    long, lat = line.split(' ')
-                    lat = float(lat)
-                    long = float(long)
+            lat_max = -90
+            lat_min = 90
+            long_max = -180
+            long_min = 180
 
-                    # Update the extremes if necessary
-                    if lat < lat_min:
-                        lat_min = lat
-                    if lat > lat_max:
-                        lat_max = lat
-                    if long < long_min:
-                        long_min = long
-                    if long > long_max:
-                        long_max = long
+            # Parse the lat/long from the file
+            with open(f'{day}/{file}', 'r') as f:
+                # Every line is a polygon
+                for line in f.readlines():
+                    # Remove the 'POLYGON ((' and '))\n' from the line
+                    line = line.removeprefix('POLYGON ((').removesuffix('))\n')
+                    coords = line.split(', ')
+                    for line in coords:
+                        long, lat = line.split(' ')
+                        lat = float(lat)
+                        long = float(long)
 
-            # Update the extremes for the fire
-            extremes[fire_name] = (lat_min, lat_max, long_min, long_max)
+                        # Update the extremes if necessary
+                        if lat < lat_min:
+                            lat_min = lat
+                        if lat > lat_max:
+                            lat_max = lat
+                        if long < long_min:
+                            long_min = long
+                        if long > long_max:
+                            long_max = long
+
+                # Update the extremes for the fire
+                extremes[fire_name] = (lat_min, lat_max, long_min, long_max)
 
 total_data = 0
 
