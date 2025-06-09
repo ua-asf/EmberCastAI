436a281cc0274d340ebf943a33cab5d1bc0ebcfc
Kaleb Burris <kaleb.r.burris@gmail.com>
2025-06-09 14:47:25 -0800

    fix: Ensured the `asf_search` query has a sufficiently large bounding box (+6km) to avoid clipped data. Sorted such that VV is written to band 2.

--- get_and_crop_sar.py
+++ get_and_crop_sar.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import asf_search as asf
 import math
-from ship import generate_cropped_geocode_grd
-from geo import crop_and_scale_to_20x20, haversine_distance, draw_wkt_to_geotiff
+from ship import generate_geocoded_grd
+from geo import crop_and_scale_to_20x20, haversine_distance, draw_wkt_to_geotiff, merge_geotiffs
 from shapely.geometry import Polygon
+import shutil
 
 # GDAL configuration
 from osgeo import gdal
@@ -13,6 +14,12 @@ gdal.UseExceptions()
 
 # Dates used by the dataset are usually in the format YYYYMMDD
 date_format_str = '%Y-%m-%d'
+
+# The width/height of the pixels in the SAR data, in meters
+PIXEL_SIZE = 20
+
+# The width/height of the square in the SAR data, in pixels (as referenced above)
+SQUARE_SIZE = 100
 
 # Fires is a dict of each fire, with each fire being a 
 # list of the files associated with that fire, sorted
@@ -47,7 +54,7 @@ for fire in dirnames:
         # Check if the directory is a fire
         if os.path.isdir(os.path.join('organized_dataset', fire)):
             # Remove the directory
-            os.system(f'rm -rf organized_dataset/{fire}')
+            shutil.rmtree(f'organized_dataset/{fire}')
             print(f'Removed {fire}')
 
 print(f'Found {len(fires)} fires')
@@ -68,16 +75,16 @@ for fire, data in fires.items():
 extremes = fires.copy()
 
 for fire_name, data in fires.items():
+    lat_min = 90
+    lat_max = -90
+    long_min = 180
+    long_max = -180
+
     for day in data:
         for file in os.listdir(day):
             # Only open the .wkt files
             if not file.endswith('.wkt'):
                 continue
-
-            lat_max = -90
-            lat_min = 90
-            long_max = -180
-            long_min = 180
 
             # Parse the lat/long from the file
             with open(f'{day}/{file}', 'r') as f:
@@ -100,36 +107,40 @@ for fire_name, data in fires.items():
                         if long < long_min:
                             long_min = long
                         if long > long_max:
-                            long_max = long
+                            long_max = long                
 
-                
+    # Scale the extremes to be a multiple of SQUARE_SIZE * PIXEL_SIZE meters, in a square
 
-                # Scale the extremes to be a multiple of 2000x2000 meters, in a square
-                # Get the distance of latitude and longitude
-                lat_dist = haversine_distance(lat_min, long_min, lat_max, long_min)
-                long_dist = haversine_distance(lat_min, long_min, lat_min, long_max)
+    # Get the distance of latitude and longitude
+    lat_dist = haversine_distance(lat_min, long_min, lat_max, long_min)
+    long_dist = haversine_distance(lat_min, long_min, lat_min, long_max)
 
-                # Get the larger of the two differences
-                width = max(lat_dist, long_dist)
+    # Get the larger of the two differences
+    width = max(lat_dist, long_dist)
 
-                # Find the nearest multiple of 2000 meters
-                new_width = math.ceil(width / 1000) * 1000
+    # Get the square size in meters
+    square_size = SQUARE_SIZE * PIXEL_SIZE
 
-                # Adjust the extremes up and down to fit the new width, back into lat/long
-                # Get the difference between the new width and the old width
-                new_lat_diff = (new_width - lat_dist) / 2
-                new_long_diff = (new_width - long_dist) / 2
+    # Find the nearest multiple of SQUARE_SIZE * PIXEL_SIZE meters + some extra space
+    new_width = (math.ceil(width / square_size) + 3) * square_size
 
-                r_earth = 6378000
+    print(f'New width: {new_width}')
 
-                # Adjust the extremes up and down to fit the new width, back into lat/long
-                lat_min = lat_min - (new_lat_diff / r_earth) * (180 / math.pi)
-                lat_max = lat_max + (new_lat_diff / r_earth) * (180 / math.pi)
-                long_min = long_min - (new_long_diff / r_earth) * (180 / math.pi)
-                long_max = long_max + (new_long_diff / r_earth) * (180 / math.pi)
+    # Adjust the extremes up and down to fit the new width, back into lat/long
+    # Get the difference between the new width and the old width
+    new_lat_diff = (new_width - lat_dist) / 2
+    new_long_diff = (new_width - long_dist) / 2
 
-                # Update the extremes for the fire
-                extremes[fire_name] = (lat_min, lat_max, long_min, long_max)
+    r_earth = 6378137
+
+    # Adjust the extremes up and down to fit the new width, back into lat/long
+    lat_min = lat_min - (new_lat_diff / r_earth) * (180 / math.pi)
+    lat_max = lat_max + (new_lat_diff / r_earth) * (180 / math.pi)
+    long_min = long_min - (new_long_diff / r_earth) * (180 / math.pi)
+    long_max = long_max + (new_long_diff / r_earth) * (180 / math.pi)
+
+    # Update the extremes for the fire
+    extremes[fire_name] = (lat_min, lat_max, long_min, long_max)
 
 # Print the extremes for each fire
 for fire, data in extremes.items():
@@ -172,8 +183,8 @@ for fire, data in extremes.items():
 
         print(f'{fire}: {fires[fire]}')
 
-        date = min(fires[fire], key=lambda x: datetime.strptime(x.split('/')[-1], date_format_str))
-        date = datetime.strptime(date.split('/')[-1], date_format_str)
+        date = min(fires[fire], key=lambda x: datetime.strptime(os.path.basename(x), date_format_str))
+        date = datetime.strptime(os.path.basename(date), date_format_str)
 
         print(f'Getting data for {fire} on {date}')
 
@@ -181,7 +192,7 @@ for fire, data in extremes.items():
 
         try:
 
-            polygon = f'POLYGON(({data[2]:.6} {data[1]:.6}, {data[2]:.6} {data[0]:.6}, {data[3]:.6} {data[0]:.6}, {data[3]:.6} {data[1]:.6}, {data[2]:.6} {data[1]:.6}))'
+            polygon = f'POLYGON(({data[2]:.8} {data[1]:.8}, {data[2]:.8} {data[0]:.8}, {data[3]:.8} {data[0]:.8}, {data[3]:.8} {data[1]:.8}, {data[2]:.8} {data[1]:.8}))'
 
             print(f'Polygon: {polygon}')
 
@@ -231,13 +242,15 @@ for fire, data in extremes.items():
                         target = [[float(coord[0]), float(coord[1])] for coord in target]
                         
                         # Convert the coordinates to a polygon
-                        target_polygon = Polygon(candidate)
+                        candidate_polygon = Polygon(candidate)
                         fire_polygon = Polygon(target)
 
                         # Check if the coordinates are within the bounds of the fire
                         # The order goes [0] = top left, [1] = top right, [2] = bottom right, [3] = bottom left
-                        if target_polygon.contains(fire_polygon):
+                        if candidate_polygon.contains_properly(fire_polygon):
                             print(f'Found {result.properties['sceneName']}')
+                            print(f'Candidate polygon: {candidate_polygon}')
+                            print(f'Fire polygon: {fire_polygon}')
                             results = [result]
                             break
                         else:
@@ -251,7 +264,7 @@ for fire, data in extremes.items():
             if not os.path.isfile(f'tmp/{results[0].properties['sceneName']}.zip'):
                 results[0].download(path='tmp', session=session)
 
-            files = generate_cropped_geocode_grd(results[0].properties['sceneName'], out_dir=f'organized_dataset/{fire}/data')
+            files = generate_geocoded_grd(results[0].properties['sceneName'], out_dir=f'organized_dataset/{fire}/data')
 
             print(f'Files: {files}')
 
@@ -263,17 +276,35 @@ for fire, data in extremes.items():
                     nw_latlon=(data[1], data[2]), 
                     sw_latlon=(data[0], data[2]), 
                     se_latlon=(data[0], data[3]), 
-                    ne_latlon=(data[1], data[3]), 
+                    ne_latlon=(data[1], data[3]),
+                    pixel_size=PIXEL_SIZE,
+                    square_size=SQUARE_SIZE,
                 )
             
         except Exception as e:
             print(f'Error with {fire}: {e}')
             continue
 
+# Merge all geotiffs into a single geotiff
+for fire, data in fires.items():
+    # Get the SAR data files (.tiff)
+    sar_files = [file for file in os.listdir(f'organized_dataset/{fire}/data') if file.endswith('.tiff') and file != 'merged.tiff']
+
+    # Sort such that the vv band is first, then the vh band
+    if 'vh' in sar_files[0]:
+        sar_files = sar_files[::-1]
+
+    # Append full file path to each file
+    sar_files = [os.path.join(f'organized_dataset/{fire}/data', file) for file in sar_files]
+
+    # Merge the files into a single geotiff
+    merge_geotiffs(sar_files, output_file=f'organized_dataset/{fire}/data/merged.tiff')
+
+
 # Put the WKT fire pixel polygons onto the GeoTIFF
 for fire, data in fires.items():
     # Get the SAR data files (.tiff)
-    sar_files = [file for file in os.listdir(f'organized_dataset/{fire}/data') if file.endswith('.tiff')]
+    sar_files = [file for file in os.listdir(f'organized_dataset/{fire}/data') if file == 'merged.tiff']
 
     for day in data:
         # Search for the .wkt file for the fire polygon
