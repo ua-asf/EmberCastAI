15cf90b59a36c1a13a2084b918ac2a1fd34b8ad0
Kaleb Burris <kaleb.r.burris@gmail.com>
2025-07-31 09:47:00 -0800

    fix: Add path variables for GTK and XDG

--- EmberCastAIGUI/shell.nix
+++ EmberCastAIGUI/shell.nix
@@ -20,6 +20,8 @@ pkgs.mkShell {
   OPENSSL_DIR = "${pkgs.openssl.out}/lib";
   OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
   PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  GTK_PATH="${pkgs.libcanberra-gtk3}/lib/gtk-3.0";
+  XDG_DATA_DIRS="${pkgs.gtk3}/share:${pkgs.libcanberra}/share:$XDG_DATA_DIRS";
 
   shellHook = ''
     rustup default stable
