5dd2930767a4c9b328ad8aaafc299864991bdf33
Kaleb Burris <kaleb.r.burris@gmail.com>
2025-11-10 16:05:42 -0900

    chore: Add dockerfile for non-Nix users

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,12 @@
 #! bash
-nix build #.docker
-sudo docker load <result
+
+if command -v "nix" >/dev/null 2>&1; then
+  echo "Building with nix."
+  nix build #.docker
+  sudo docker load <result
+else
+  echo "Building with Docker."
+  docker build . -t fastapi-app
+fi
+
 sudo docker run -v ./env.sh:/app/env.sh:ro -p 8000:8000 fastapi-app:latest
